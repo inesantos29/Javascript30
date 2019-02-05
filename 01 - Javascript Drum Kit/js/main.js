d0335a65bd0b5412c24e87e3f0d4657d8030c921
inesantos29 <ines.arts@hotmail.com>
2019-02-05 12:21:06 +0000

    Add event listener keydown

--- 01 - Javascript Drum Kit/js/main.js
+++ 01 - Javascript Drum Kit/js/main.js
@@ -1,4 +1,8 @@
 window.addEventListener('keydown', function (e){
    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-   console.log(audio);
+   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+   if(!audio) return; // stop the function from running all together
+   key.classList.add('playing');
+   audio.currentTime = 0; // rewind to the start
+   audio.play()
 });
