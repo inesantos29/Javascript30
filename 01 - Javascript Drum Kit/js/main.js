f6f2587d8dd9a9091bc86ecc474e13d5d26ebe40
inesantos29 <ines.arts@hotmail.com>
2019-02-06 15:09:36 +0000

    Added the remove and play function

--- 01 - Javascript Drum Kit/js/main.js
+++ 01 - Javascript Drum Kit/js/main.js
@@ -1,8 +1,20 @@
-window.addEventListener('keydown', function (e){
+
+function playSound(e){
    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
    if(!audio) return; // stop the function from running all together
-   key.classList.add('playing');
    audio.currentTime = 0; // rewind to the start
-   audio.play()
-});
+   audio.play();
+   key.classList.add('playing');
+}
+
+function  removeTransition(e) {
+  if (e.propertyName !== 'transform') return; //skip it if it's not a transform
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
+
